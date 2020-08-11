3d265861ca28fb56f2c5278d536f1ac55aafcf55
Boris Schrijver <bschrijver@schubergphilis.com>
2020-08-11 17:32:21 +0200

    Hotfix for kepler environment

--- versions.tf
+++ versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    mcaf = {
+      source  = "your-registry.example.com/organization/mcaf"
+      version = "0.1.0"
+    }
+  }
 }
